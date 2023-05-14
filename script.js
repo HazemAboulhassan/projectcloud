e7b40d8cf5c7504c7c0ae3a03de3a3fe8c3d7694
hazemaboulhassan <hazemaboulhassan8@gmail.com>
2023-05-14 16:01:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+document.getElementById('new').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var form = event.target;
+    var data = {
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        age: form.elements.age.value,
+        gender: form.elements.gender.value
+    };
+    fetch('/persons', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(function(response) {
+            if (response.ok) {
+                window.location.reload();
+                alert('Done!')
+            } else {
+                alert('Error adding person');
+            }
+        });
+});
+
+// Update a person
+document.getElementById('update').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var form = event.target;
+    var id = form.elements.id.value;
+    var data = {
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        age: form.elements.age.value,
+        gender: form.elements.gender.value
+    };
+    fetch('/persons/' + id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(function(response) {
+            if (response.ok) {
+                window.location.reload();
+            } else {
+                alert('Error updating person');
+            }
+        });
+});
+
+// Delete a person
+document.getElementById('delete').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var form = event.target;
+    var id = form.elements.id.value;
+    fetch('/persons/' + id, {
+            method: 'DELETE'
+        })
+        .then(function(response) {
+            if (response.ok) {
+                window.location.reload();
+            } else {
+                alert('Error deleting person');
+            }
+        });
+});
